fddf9fb5d42999051a55bc353a9eff33721967c7
Toshka <tkasuga@gmail.com>
2026-01-03 17:11:14 +0900

    今日の進捗

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -1,18 +1,35 @@
 ;;; init-dev.el --- Programming & Development -*- lexical-binding: t -*-
 
-;; Git連携 (Magit)
+;; 1. Git連携 (Magit)
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
 
-;; Tree-sitter (構文解析)
+
+
+;; 3. 洗練された補完 UI (Corfu)
+;; 入力中に邪魔にならない程度に候補を表示してくれる、モダンな補完ツールです
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-auto t)                 ; 自動で候補を出す
+  (corfu-auto-delay 0.1)         ; 0.1秒待って表示
+  (corfu-auto-prefix 2)          ; 2文字打ったら表示
+  (corfu-quit-at-boundary nil)   ; スペースを打っても消えない
+  :init
+  (global-corfu-mode))
+
+;; 4. 構文チェックの標準化 (Flymake)
+;; Emacs標準の機能を使って、コードの書き間違いを警告します
+(add-hook 'prog-mode-hook 'flymake-mode)
+
+;; 5. Tree-sitter & Eglot (以前の設定を維持)
 (use-package treesit
   :ensure nil
   :if (boundp 'treesit-font-lock-level)
   :config
   (setq treesit-font-lock-level 4))
 
-;; Eglot (LSPクライアント)
 (use-package eglot
   :ensure nil
   :hook
@@ -21,7 +38,7 @@
   :config
   (keymap-set eglot-mode-map "s-." 'eglot-code-actions))
 
-;; Symbol Overlay (変数ハイライト)
+;; 6. 変数のハイライト
 (use-package symbol-overlay
   :ensure t
   :bind ("M-i" . symbol-overlay-put))
